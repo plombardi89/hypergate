9b4c72a3c45ad90bf96efe68c91133c5f6cb0f41
plombardi <plombardi89@gmail.com>
2018-03-06 03:54:00 -0500

    Pull in the server dependency as there is no reason to write one.
    - switch to a newer netty version for JDK9 reasons
    - logback configuration

--- src/main/kotlin/hypergate/Hypergate.kt
+++ src/main/kotlin/hypergate/Hypergate.kt
@@ -1,6 +1,43 @@
 package hypergate
 
+import io.envoyproxy.controlplane.cache.*
+import io.envoyproxy.controlplane.server.DiscoveryServer
+import io.grpc.Server
+import io.grpc.netty.NettyServerBuilder
+import org.slf4j.LoggerFactory
+
+
+private val logger = LoggerFactory.getLogger("hypergate.Hypergate")
+
 
 fun main(args: Array<String>) {
-  println("Hello, world!")
+  val server = createServer()
+
+  server.start()
+  logger.info("Hypergate has started 'port = {}'!", server.port)
+
+  Runtime.getRuntime().addShutdownHook(Thread {
+    logger.info("Hypergate is shutting down...")
+    server.shutdown()
+    logger.info("Hypergate has shutdown!")
+  })
+
+  server.awaitTermination()
 }
+
+
+private fun createServer(): Server {
+  val cache = SimpleCache(null) { _ -> "KEY" }
+
+  val discoveryServer = DiscoveryServer(cache)
+
+  return with(NettyServerBuilder.forPort(7000)) {
+    addService(discoveryServer.aggregatedDiscoveryServiceImpl)
+    addService(discoveryServer.clusterDiscoveryServiceImpl)
+    addService(discoveryServer.endpointDiscoveryServiceImpl)
+    addService(discoveryServer.listenerDiscoveryServiceImpl)
+    addService(discoveryServer.routeDiscoveryServiceImpl)
+    build()
+  }
+}
+
